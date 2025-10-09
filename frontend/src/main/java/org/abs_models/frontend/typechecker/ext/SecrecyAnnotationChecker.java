914316ec9e6b273066092cd5835934828125c01c
Disakek <maximilianpaul1811@gmail.com>
2025-10-09 20:52:56 +0200

    changing interfacedecl to support secrecy annotations for method parameters and method return values

--- frontend/src/main/java/org/abs_models/frontend/typechecker/ext/SecrecyAnnotationChecker.java
+++ frontend/src/main/java/org/abs_models/frontend/typechecker/ext/SecrecyAnnotationChecker.java
@@ -15,48 +15,91 @@ import org.abs_models.frontend.typechecker.TypeAnnotation;
 import org.abs_models.frontend.typechecker.ext.DefaultTypeSystemExtension;
 import org.abs_models.frontend.typechecker.ext.AdaptDirection;
 
-
 public class SecrecyAnnotationChecker extends DefaultTypeSystemExtension {
 
     protected SecrecyAnnotationChecker(Model m) {
         super(m);
     }
 
+    /*
+    for (MethodSig sig : decl.getMethodList()) {
+            // Check return type annotations
+            for (Annotation ann : sig.getTypeUse().getAnnotationList()) {
+                handleAnnotation(ann, sig);
+            }
+
+            // Check parameter annotations
+            for (ParamDecl param : sig.getParamList()) {
+                for (Annotation ann : param.getAnnotationList()) {
+                    handleAnnotation(ann, param);
+                }
+            }
+        }
+    */
+
     @Override
     public void checkInterfaceDecl(InterfaceDecl decl) {
-        for (Annotation annotation : decl.getAnnotations()) {
-            
-            if (annotation instanceof TypedAnnotation typedAnn) {
-                    PureExp secrecy = typedAnn.getValue(); // Extract the value safely
-                    checkSecrecyAnnotationCorrect(decl, secrecy);
-            }
 
-            for (int i = 0; i < annotation.getNumChild(); i++) {
-                ASTNode<?> child = annotation.getChild(i);
+        //if (decl.getModuleDecl().getName().equals("Test")) { //Helper stmt to ignore inherited classes from .Object class
+            for (MethodSig sig : decl.getBodyList()) {
+                
+                //System.out.println(sig.toString());  //Prints the MethodSig
 
-                if (child instanceof IntLiteral intLit) {
-                    // Look at the other child to ensure it's a Secrecy annotation
-                    ASTNode<?> otherChild = annotation.getChild(1 - i); // the child that is not intLit
-                    if (otherChild.toString().equals("Secrecy")) {
-                        int level = Integer.parseInt(intLit.getContent());
-                        System.out.println("Secrecy level: " + level);
+                TypeUse returnType = sig.getReturnType();
+                
+                for (Annotation annotation : returnType.getAnnotationList()) {
+                    //System.out.println(annotation); //Prints the Annotations for the ReturnValues
 
-                        // Save to metadata
-                        System.out.println("TODO MISSING METADATA ATTACHEMENT");
+                    extractAnnotationSafely(returnType, annotation); //Helper Function to extract and then check the SecrecyValue
+                }
 
-                        decl.getType().addMetaData("Secrecy", level);
+                for (ParamDecl parameter : sig.getParamList()) {
+                
+                    //System.out.println("  Param: " + parameter.getName() + " : " + parameter.getTypeUse().getName()); //Prints the Parameter Name and Type
 
-                        System.out.println("Should be added");
+                    for(Annotation annotation : parameter.getAnnotationList()){
+                        //System.out.println(annotation); //Prints the Annotations for the ParameterValues
 
-                        System.out.println(decl.getType().getMetaData("Secrecy"));
+                        extractAnnotationSafely(parameter.getTypeUse(), annotation); //Helper Function to extract and then check the SecrecyValue
                     }
                 }
-                
             }
+        //}
+    }
+
+    //Extracts the PureExp from an Annotation after running the checkSecrecyAnnotationCheck it extracts the value and adds it as metadata to the node
+    private void extractAnnotationSafely(TypeUse typeU, Annotation annotation) {
+
+        if (annotation instanceof TypedAnnotation typedAnnotation) {
+
+            PureExp secrecy = typedAnnotation.getValue(); // Extract the value safely
+
+            checkSecrecyAnnotationCorrect(typeU, secrecy);
+        }
+
+        for (int i = 0; i < annotation.getNumChild(); i++) {
+                ASTNode<?> child = annotation.getChild(i);
+
+            if (child instanceof IntLiteral intLit) {
+            
+            // Look at the other child to ensure it's a Secrecy annotation
+            
+            ASTNode<?> otherChild = annotation.getChild(1 - i); // the child that is not intLit
+
+                if (otherChild.toString().equals("Secrecy")) {
+
+                    int level = Integer.parseInt(intLit.getContent());
+
+                    typeU.getType().addMetaData("Secrecy", level); //Add to metadata of the node
+                    System.out.println(typeU.getType().getMetaData("Secrecy")); //Check if it got attached right
+                }
+            }
+
         }
     }
 
-    // Checks that the input secrecy is a number and that it is neither 0 nor null
+    // Checks that the input secrecy is a number and that it is not null
+    // TODO: remove the option to set the secrecy to 0 (assume / set this as base value later)
     private void checkSecrecyAnnotationCorrect(ASTNode<?> n, PureExp secrecy) {
         //System.out.println("Called Secrecy Correct");
         if (secrecy == null) return;
